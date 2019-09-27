2818db2981128eeadbd0e011e8e2244ac4472738
Manisha Korala <tanwani.manisha@gmail.com>
2019-09-27 22:12:13 +0500

    add MySQL script to create DB

---
+++ adjust.sql
@@ -0,0 +1,17 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema performance_metric
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema performance_metric
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `performance_metric` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema new_schema1
+-- -----------------------------------------------------
